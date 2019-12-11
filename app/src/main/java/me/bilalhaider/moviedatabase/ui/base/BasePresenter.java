9fda7d66cff6a88c88e9693496d26b6c9b7deec8
Bilal <bhdevelopment92@gmail.com>
2019-12-11 11:30:04 +0000

    feature: finished BasePresenter

--- app/src/main/java/me/bilalhaider/moviedatabase/ui/base/BasePresenter.java
+++ app/src/main/java/me/bilalhaider/moviedatabase/ui/base/BasePresenter.java
@@ -1,10 +1,52 @@
 package me.bilalhaider.moviedatabase.ui.base;
 
-import androidx.lifecycle.LifecycleEventObserver;
+import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.LifecycleObserver;
+import androidx.lifecycle.OnLifecycleEvent;
+
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
 
 public class BasePresenter<V extends BaseView> implements LifecycleObserver {
 
-    private V mCurrentView;
+    public V mCurrentView;
+    private CompositeDisposable mCompositeDisposable;
+
+    public void attachView(V view) {
+        if (mCurrentView != null) {
+            detachView();
+        }
+        mCurrentView = view;
+    }
+
+    public void detachView() {
+        mCurrentView = null;
+    }
+
+    public void initCompositDisposible() {
+        if (mCompositeDisposable.isDisposed()) {
+            mCompositeDisposable = new CompositeDisposable();
+        }
+    }
+
+    public void clearCompositeDisposable() {
+        mCompositeDisposable.dispose();
+    }
+
+    public void addDisposable(Disposable disposable) {
+        mCompositeDisposable.add(disposable);
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    public void presenterOnDestroy() {
+        clearCompositeDisposable();
+        detachView();
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    public void presenterOnCreate() {
+        initCompositDisposible();
+    }
+
 
 }
