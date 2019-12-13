146bc23633d9612a0bf0f5bbd5e7a3debcfdea22
Bilal <bhdevelopment92@gmail.com>
2019-12-13 10:56:35 +0000

    feature: creating the presenter

--- app/src/main/java/me/bilalhaider/moviedatabase/ui/home/HomeContract.java
+++ app/src/main/java/me/bilalhaider/moviedatabase/ui/home/HomeContract.java
@@ -1,4 +1,20 @@
 package me.bilalhaider.moviedatabase.ui.home;
 
+import me.bilalhaider.moviedatabase.data.model.SearchResponse;
+import me.bilalhaider.moviedatabase.ui.base.BaseView;
+
 public interface HomeContract {
+
+    interface View extends BaseView {
+        Boolean isNetworkAvailable();
+        void showProgress();
+        void hideProgress();
+        void handleOnResponseSuccess(SearchResponse searchResponse);
+        void handleOnErrorResponse(String errorMessage);
+    }
+
+    interface Presenter {
+        void searchMovie(String searchQuery);
+    }
+
 }
--- app/src/main/java/me/bilalhaider/moviedatabase/ui/home/HomePresenter.java
+++ app/src/main/java/me/bilalhaider/moviedatabase/ui/home/HomePresenter.java
@@ -1,4 +1,48 @@
 package me.bilalhaider.moviedatabase.ui.home;
 
-public class HomePresenter {
+import io.reactivex.Single;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.observers.DisposableSingleObserver;
+import io.reactivex.schedulers.Schedulers;
+import me.bilalhaider.moviedatabase.MovieDatabaseApplication;
+import me.bilalhaider.moviedatabase.data.model.SearchResponse;
+import me.bilalhaider.moviedatabase.ui.base.BasePresenter;
+
+public class HomePresenter extends BasePresenter<HomeContract.View> implements HomeContract.Presenter {
+
+    @Override
+    public void searchMovie(String searchQuery) {
+        if (mCurrentView != null) {
+            if (mCurrentView.isNetworkAvailable()) {
+                mCurrentView.showProgress();
+                Single<SearchResponse> searchMovies = MovieDatabaseApplication.getApiService().getSearchResults(searchQuery);
+                SearchResultObserver searchResultObserver = new SearchResultObserver();
+                searchMovies.subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(searchResultObserver);
+            } else {
+                mCurrentView.handleOnErrorResponse("");
+            }
+        }
+
+    }
+
+    class SearchResultObserver extends DisposableSingleObserver<SearchResponse> {
+
+        @Override
+        public void onSuccess(SearchResponse value) {
+            if (mCurrentView != null) {
+                mCurrentView.hideProgress();
+                mCurrentView.handleOnResponseSuccess(value);
+            }
+        }
+
+        @Override
+        public void onError(Throwable e) {
+            if (mCurrentView != null) {
+                mCurrentView.hideProgress();
+                mCurrentView.handleOnErrorResponse(e.getLocalizedMessage());
+            }
+        }
+    }
 }
