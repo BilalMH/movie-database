0346a192a6d1aaaf67167f7b78f47ba5b7279d01
Bilal Haider <bhdevelopment92@gmail.com>
2022-03-17 13:51:52 +0000

    created buildSrc folder for library/project extensions and Versions for dependencies

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,18 @@
+/**
+ * Created by Bilal Haider on 17/03/2022
+ */
+object Versions {
+    const val MIN_ANDROID_SDK = 24
+    const val TARGET_ANDROID_SDK = 31
+
+    object Compose {
+        const val COMPOSE = "1.0.5"
+        const val COMPOSE_COMPILER = "1.1.0-rc02"
+        const val PAGING_COMPOSE = "1.0.0-alpha14"
+        const val VIEWMODEL_COMPOSE = "2.4.1"
+    }
+
+    const val KTOR = "1.6.7"
+    const val JUNIT = "4.13.2"
+    const val OK_HTTP = "4.9.3"
+}
