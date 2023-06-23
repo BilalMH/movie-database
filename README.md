4d386abb891a6ec3ad79d8e633df2d4387a2e51f
Bilal Haider <bilal@seeyouonlove.com>
2023-06-23 18:13:20 +0200

    Clean up unused imports and updated README

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 Movie Database application originally in Java using MVVM now refactored to Kotlin with KMM and Jetpack Compose for UI. 
 
+This application uses Bitrise for it's CI/CD 
+
 Trello board for progress: https://trello.com/b/XAbBo0Uj/movie-database-refactor
--- app-android/src/androidTest/java/me/bilalhaider/moviedatabase/main/MainActivityTest.kt
+++ app-android/src/androidTest/java/me/bilalhaider/moviedatabase/main/MainActivityTest.kt
@@ -1,7 +1,6 @@
 package me.bilalhaider.moviedatabase.main
 
 import androidx.lifecycle.Lifecycle
-import androidx.test.core.app.ActivityScenario
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
